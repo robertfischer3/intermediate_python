fa8b576342fab343792d0e5b31c221ce00e55232
robertfischer3 <robert@oakleyrain.com>
2020-07-30 06:02:07 -0700

    Added more decorator lab excercises

--- tests/test_decorator_lab.py
+++ tests/test_decorator_lab.py
@@ -3,7 +3,7 @@ from intermediate.excercises.decorator_lab import decorator_function
 
 
 class Test(TestCase):
-    def test_decorator_function(self):
+    def test_decorator_function_a(self):
         def display():
             print("display function ran")
 
@@ -11,5 +11,18 @@ class Test(TestCase):
         wrapped_function = decorator_function(display)
         wrapped_function()
 
-    def test_decorator(self):
+    def test_decorator_function_b(self):
+        # create a function in decorator_lab module that mimics decorator_function
+
+        self.assertTrue(False)
+
+    def test_decorator_class_a(self):
+        # 1. Create and call a few functions by instantiating the decorator_class with those methods
+
+        self.assertTrue(False)
+
+    def test_decorator_class_a(self):
+        # 2. Create a simple class with a method. Instantiate the decorator_class with
+        #  the method of the simple class.
+
         self.assertTrue(False)
